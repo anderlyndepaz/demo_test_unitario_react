edcd15f4fda13abdb0c33f49e50f17b4729175e1
AnderlynDePaz <anderlyndepazhurtado@gmail.com>
2024-11-29 10:49:29 +0100

    add/solution

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
 
   return (
     <>
+    <h1>Bolito te queremos</h1>
       <div>
         <a href="https://vitejs.dev">
           <img src={viteLogo} className="logo" alt="Vite logo" />
